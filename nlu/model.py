b39620a157d59c1637ab3428eeaab8b937b55d96
REBjr13 <rebcave8@gmail.com>
2022-02-05 01:49:06 +0100

    ,

--- nlu/model.py
+++ nlu/model.py
@@ -1,14 +1,33 @@
 import yaml
 import os
 import json
+import numpy as np
 
 data = yaml.safe_load(open('nlu\\train.yaml').read())
+
+#reading the data
 
 inputs, outputs = [], []
 
 for command in data['commands']: 
-    input.append(command['input'])
-    #outputs.append('{}\{}'.format(command['entity'], command('action')))
+    inputs.append(command['input'])
+    outputs.append('{}\{}'.format(command['entity'], command['action']))
     
-print(inputs)
-#print(outputs)
+# create a data set  
+# choose a leel of tokenization
+
+chars = set()
+
+#read all chars in the dataset
+
+for i in inputs + outputs:
+    for ch in i:
+        if ch not in chars:
+            chars.add(ch)
+            
+# create input data
+
+max_sent = max([len(x) for x in inputs])
+    
+#print(len(chars))
+print('Max input seq:', max_sent)
--- nlu/test_data.py
+++ nlu/test_data.py
@@ -6,8 +6,3 @@ data = yaml.safe_load(open('nlu\\train.yaml').read())
 
 for command in data['commands']:
     print(command)
-
-#with open(r'C:\Users\pc\Desktop\hello\nlu\train.yaml') as file:
- #   train_list = yaml.load(file, Loader=yaml.FullLoader)
-    
-   # print(train_list)
